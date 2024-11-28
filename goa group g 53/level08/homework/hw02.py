2741d93e0ede097e779dda4968fa7962c34983f2
Syoma13-del <petr92034@gmail.com>
2024-11-28 20:14:05 +0400

    Update hw02.py

--- goa group g 53/level08/homework/hw02.py
+++ goa group g 53/level08/homework/hw02.py
@@ -2,10 +2,7 @@ name = input("what is your name?")
 surname = input("What is your surname?")
 age = int(input("How old are you?"))
 age = age
-login = input("what is your username?")
-password = input("what is your password?")
+
 
 print("Hello" + name + surname) 
 print("You are "+ str(age)+" years old") 
-print("correct your username is" + login)
-print("correct your password is" + password)
