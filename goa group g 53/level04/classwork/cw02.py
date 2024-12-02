6e11ada2155082ef61866375489289eeb84c3600
Syoma13-del <petr92034@gmail.com>
2024-12-02 20:41:25 +0400

    Add files via upload

---
+++ goa group g 53/level04/classwork/cw02.py
@@ -0,0 +1,2 @@
+str = (5 + 3) * 2-10 / 2 = 3 ** 2 % 4
+print("output of the large expression:", str)
---
+++ goa group g 53/level08/classwork/class01.py
@@ -0,0 +1,9 @@
+name=input("name")
+
+print(input("name"))
+name1 = input("randomized word")
+name2 = input("randomized word 02")
+
+print(name1 + name2)
+
+#input = informaciis shetana kompiutershi
