9c4ff54d41ebc15afcdd4dc2ae18d1f3ac71d1e7
Syoma13-del <petr92034@gmail.com>
2024-12-02 20:34:56 +0400

    Delete goa group g 53 directory

--- goa group g 53/level08/classwork/class01.py
+++
@@ -1,9 +0,0 @@
-name=input("name")
-
-print(input("name"))
-name1 = input("randomized word")
-name2 = input("randomized word 02")
-
-print(name1 + name2)
-
-#input = informaciis shetana kompiutershi
