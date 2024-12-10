f790672a346698da34539078de956ed36fc99d0c
Syoma13-del <petr92034@gmail.com>
2024-12-10 19:57:49 +0400

    Add files via upload

---
+++ goa group g 53/level06/classwork/class06(2).py
@@ -0,0 +1,12 @@
+Name = "Zuka"     
+user-name = "Ana" 
+For = 10                  
+Class = "Math"             
+print("userage") 
+age = 25
+age = age + "years"          
+Name = "Zuka"
+print("name")                  
+full_name = "Zuka K"         
+def_= "Function"           
+UserAge = 20
