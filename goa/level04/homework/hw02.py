277f600f5a2724c1be6d5fd4065a8665d90388af
Syoma13-del <petr92034@gmail.com>
2025-02-01 12:50:34 +0400

    Add files via upload

---
+++ goa/level04/homework/hw02.py
@@ -0,0 +1,35 @@
+a = 5
+b = 3
+result = a + b
+print(result)  # 8
+
+
+a = 8
+b = 3
+result = a - b
+print(result)  # 5
+
+a = 4
+b = 3
+result = a * b
+print(result)  # 12
+
+a = 10
+b = 2
+result = a / b
+print(result)  # 5.0
+
+
+
+
+a = 10
+b = 3
+result = a % b
+print(result)  # 1
+
+
+a = 2
+b = 3
+result = a ** b
+print(result)  # 8
+
