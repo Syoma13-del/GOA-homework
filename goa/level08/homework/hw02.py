c3c40967d53def514d04381e640f1016b070aa88
Syoma13-del <petr92034@gmail.com>
2025-02-02 19:38:21 +0400

    Delete goa directory

--- goa/level08/homework/hw02.py
+++
@@ -1,8 +0,0 @@
-name = input("what is your name?")
-surname = input("What is your surname?")
-age = int(input("How old are you?"))
-age = age
-
-
-print("Hello" + name + surname) 
-print("You are "+ str(age)+" years old") 
--- goa/level10/classwork/cw2.py
+++
@@ -1,4 +0,0 @@
-num1 = int(input("Enter your number"))
-num2 = int(input("Enter any number again"))
-
-print(num1 + num2)
